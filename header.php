26ceb111b4ecf57c6ae5b9d945e4ea6dd5e34d6a
Barry O'Rourke <barry@orodor.org.uk>
2013-05-15 21:16:27 +0100

    bring header.php in line with theme development guide

--- header.php
+++ header.php
@@ -2,26 +2,13 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
-  <title><?php
-        global $page, $paged;
-
-        wp_title( '|', true, 'right' );
-        bloginfo( 'name' );
-
-        $site_description = get_bloginfo( 'description', 'display' );
-        if ( $site_description && ( is_home() || is_front_page() ) )
-                echo " | $site_description";
-
-        if ( $paged >= 2 || $page >= 2 )
-                echo ' | ' . sprintf( __( 'Page %s', 'orodor' ), max( $paged, $page ) );
-
-        ?></title>
+  <title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
+  <meta name="description" content="<?php bloginfo( 'description' ); ?>">
   <link rel="profile" href="http://gmpg.org/xfn/11" />
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/normalise.css" />
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url');?>/css/style.css" />
   <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu" />
   <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lobster" />
-
   <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico" />
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
   <?php wp_head(); ?>
