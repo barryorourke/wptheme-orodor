3880851bba9cc04045ba25f0c9a66e5522815237
Barry O'Rourke <barry@orodor.org.uk>
2013-05-18 17:49:11 +0100

    tidy up everything search related

--- search.php
+++ search.php
@@ -1,18 +1,20 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()): ?>
-  
-<h2>Search Results for '<?php the_search_query(); ?>'</h2>
+  <?php if (have_posts()): ?>
 
-  <?php while ( have_posts() ) : the_post(); ?>
-  <?php get_template_part( 'content', 'search' ); ?>
-  <?php endwhile; ?>
+    <header><h2>Search Results for '<?php the_search_query(); ?>'</h2></header>
 
-<?php else: ?>
-<h2>Nothing found...</h2>
-<p>Sorry, but nothing matched your search criteria. Please try again with some different keywords.</p>
-<?php get_search_form(); ?>
-<?php endif; ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+      <?php get_template_part( 'content', 'search' ); ?>
+    <?php endwhile; ?>
+
+  <?php else: ?>
+
+    <header><h2>Nothing found...</h2></header>
+    <p>Sorry, but nothing matched your search criteria. Please try again with some different keywords.</p>
+    <?php get_search_form(); ?>
+
+  <?php endif; ?>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
