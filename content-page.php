548a01b26316352f9f7070be0bb54319a9b9b88e
Barry O'Rourke <barry@orodor.org.uk>
2013-09-05 20:48:27 +0100

    changes to pages

--- content-page.php
+++ content-page.php
@@ -1,11 +1,6 @@
           <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <h2><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'orodor' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-            <span class="submitted"></div>
             <div class="post-body">
               <?php the_content(); ?>
-            </div>
-            <div class="post-end">
-              <div class="tags"></div>
-              <div class="links">&nbsp;</div>
             </div>
           </article>
